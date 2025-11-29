bfeaa71c6d4fcc7a689b022d4a335ee4ae349658
Tanvir Mahmud <learntanvir@gmail.com>
2025-11-29 07:34:49 +0000

    insure that user can install the werbapp as a installable app and the we

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: 'ExamWise',
   description: 'Manage your exams with ease.',
+  manifest: '/manifest.json'
 };
 
 export default function RootLayout({
@@ -28,7 +29,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="application-name" content="ExamWise" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="ExamWise" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22%234682B4%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20%22/><path d=%22m14 7 2 2-4 4%22/><path d=%22m14 7 2 2-4 4%22/><path d=%22M9.5 12.5 7 15l-2-2%22/></svg>" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+
       </head>
       <body
         className={cn(
