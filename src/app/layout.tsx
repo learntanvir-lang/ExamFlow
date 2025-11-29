9ab2e57f5420338f2d104ab950199895428922d2
Tanvir Mahmud <learntanvir@gmail.com>
2025-11-29 07:09:59 +0000

    use this icon as the website favicon (_for element <BookOpenCheck>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22%234682B4%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20%22/><path d=%22m14 7 2 2-4 4%22/><path d=%22m14 7 2 2-4 4%22/><path d=%22M9.5 12.5 7 15l-2-2%22/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
